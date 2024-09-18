07ef085cbc8f215e9fee75a32d67acb1935d8658
INagaty <ismaielnagaty@live.com>
2024-09-18 16:50:21 +0300

    Add project files.

---
+++ Class.cs
@@ -0,0 +1,16 @@
+﻿
+namespace WebApplication2_middleware_
+{
+    public class Class : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            throw new NotImplementedException();
+            await context.Response.WriteAsync("Custom Middleware incoming Request");
+            await next(context);
+            await context.Response.WriteAsync("Custom Middleware outcoming Response");
+
+        }
+
+    }
+}
